4be17e60d3ebf9344efd0c7a25bf1fceb26c575a
hoanganhphan0409 <122379219+anhphgameestudio@users.noreply.github.com>
2023-04-14 14:17:03 +0700

    add logic hook on anchor

--- Assets/Scripts/DragEndPoint.cs
+++ Assets/Scripts/DragEndPoint.cs
@@ -3,53 +3,97 @@ using System.Collections;
 using System.Collections.Generic;
 using pancake.Rope2DEditor;
 using UnityEngine;
+
 public class DragEndPoint : MonoBehaviour
 {
     private Camera _camera;
+
     [SerializeField] private RopeMaker ropeMaker;
+    [SerializeField] private LayerMask circleLayerMask;
+    [SerializeField] private CircleCollider2D circleCollider2D;
     // Start is called before the first frame update
     public int typeEnd;
+    private Vector3 StartPos;
+    private bool hooked = false;
     void Start()
     {
         _camera = ropeMaker.GetComponentInParent<Level>().Camera;
+        hooked = false;
+        ropeMaker.ground.GetComponent<BoxCollider2D>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var newLocalPos = typeEnd == 1? ropeMaker.end1 : ropeMaker.end2;
+        var newLocalPos = typeEnd == 1 ? ropeMaker.end1 : ropeMaker.end2;
         newLocalPos.z = -1;
         transform.localPosition = newLocalPos;
-        Collider[] colliders = Physics.OverlapSphere(transform.position, 1f);
-        Debug.Log(colliders.Length);
-        foreach (Collider hit in colliders)
-            Debug.Log(hit.gameObject);
+        if (hooked == true) ropeMaker.ground.GetComponent<BoxCollider2D>().enabled = true;
+        else ropeMaker.ground.GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    private void OnMouseDown()
+    {
+        if (typeEnd == 1)
+        {
+            StartPos = ropeMaker.end1;
+        }
+        else
+        {
+            StartPos = ropeMaker.end1;
+        }
     }
 
     private void OnMouseDrag()
     {
         Vector3 mousePosition = Input.mousePosition;
         Vector3 worldPosition = _camera.ScreenToWorldPoint(mousePosition);
-        
         worldPosition.z = 0;
-        if(typeEnd == 1)
-        { ropeMaker.end1 = ropeMaker.transform.InverseTransformPoint((worldPosition));
+        if (typeEnd == 1)
+        {
+            ropeMaker.end1 = ropeMaker.transform.InverseTransformPoint((worldPosition));
         }
         else
         {
             ropeMaker.end2 = ropeMaker.transform.InverseTransformPoint((worldPosition));
         }
+
         ropeMaker.CreateRope();
         ropeMaker.ground.GetComponent<BoxCollider2D>().enabled = false;
     }
 
     private void OnMouseUp()
     {
-        ropeMaker.ground.GetComponent<BoxCollider2D>().enabled = true;
-    }
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 worldPosition = _camera.ScreenToWorldPoint(mousePosition);
+        worldPosition.z = 0;
+        RaycastHit2D hit = Physics2D.CircleCast(worldPosition, circleCollider2D.radius, Vector3.forward,10f, circleLayerMask);
+        if (hit)
+        {
+            if (typeEnd == 1)
+            {
+                ropeMaker.end1 = ropeMaker.transform.InverseTransformPoint(hit.collider.gameObject.transform.position);
+            }
+            else
+            {
+                ropeMaker.end2 = ropeMaker.transform.InverseTransformPoint(hit.collider.gameObject.transform.position);
+            }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log(other.gameObject);
+            hooked = true;
+            ropeMaker.CreateRope();
+        }
+        else
+        {
+            if (typeEnd == 1)
+            {
+                ropeMaker.end1 = StartPos;
+            }
+            else
+            {
+                ropeMaker.end2 = StartPos;
+            }
+            ropeMaker.CreateRope();
+        }
+        
     }
 }
