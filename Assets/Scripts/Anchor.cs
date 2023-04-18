cfd5715d399807a23791227904110e4d54be72ab
hoanganhphan0409 <122379219+anhphgameestudio@users.noreply.github.com>
2023-04-18 15:39:00 +0700

    fix bug anchor

--- Assets/Scripts/Anchor.cs
+++ Assets/Scripts/Anchor.cs
@@ -7,24 +7,23 @@ public class Anchor : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool hasHooked = false;
+    [SerializeField] private LayerMask anchorLayerMask;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-    }
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, GetComponent<CircleCollider2D>().radius*transform.localScale.x,
+            -Vector3.forward, 10f,anchorLayerMask);
+        Debug.DrawRay(transform.position,-Vector3.forward,Color.red);
+        if (!hit)
+        {
+            hasHooked = false;
+        }
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        hasHooked = true;
-    }
 
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        hasHooked = false;
     }
 }
