2fc551899c3d8659ad962ca017c87b7161939478
hoanganhphan0409 <122379219+anhphgameestudio@users.noreply.github.com>
2023-05-18 16:52:06 +0700

    add button reset ball

--- Assets/Scripts/SpawnController/SpawnBallController.cs
+++ Assets/Scripts/SpawnController/SpawnBallController.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpawnBallController : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private GameObject Ball;
     [SerializeField] private Transform SpawnPos;
+
+    private List<GameObject> _balls = new List<GameObject>();
     // Update is called once per frame
     public void SpawnBall()
     {
         var newBall = Instantiate(Ball, LevelController.Instance.currentLevel.transform);
         newBall.layer = LayerMask.NameToLayer("BallHitPipe");
-        newBall.transform.position = SpawnPos.transform.position;
+        newBall.transform.position = SpawnPos.transform.position; 
+        _balls.Add(newBall);
+    }
 
+    public void ResetBall()
+    {
+        if (_balls.Count == 0) return;
+        for (int i = 0; i < _balls.Count; i++)
+        {
+            //_balls[i].GetComponent<TrailRenderer>().enabled = false;
+            _balls[i].SetActive(false);
+        }
+
+        StartCoroutine(DropBall());
+    }
+
+    IEnumerator DropBall()
+    {
+        int pos = 0;
+        while (pos < _balls.Count)
+        {
+            yield return new WaitForSeconds(0.1f);
+            _balls[pos].transform.position = SpawnPos.position;
+            _balls[pos].layer = LayerMask.NameToLayer("BallHitPipe");
+            _balls[pos].SetActive(true);
+            pos++;
+        }
     }
 }
